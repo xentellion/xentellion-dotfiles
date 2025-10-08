058e3e872173ef0a5ca125b6c09aa0d74d522a1b
xentellion <d.g.mchedlishvili@gmail.com>
2025-10-08 16:39:12 +0300

    fox:wofi and weather

--- hypr/bin/weather.sh
+++ hypr/bin/weather.sh
@@ -1,1 +1,8 @@
-curl wttr.in/Moscow?format=3
+#!/bin/sh
+
+if ping -q -c 1 -W 1 wttr.in >/dev/null; then
+    WEATHER=$(curl wttr.in/Moscow?format=3)
+    echo $WEATHER
+else
+    echo "Moscow: ?? ???°C"
+fi
